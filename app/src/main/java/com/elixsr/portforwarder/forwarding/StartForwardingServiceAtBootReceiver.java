f3c1fd1a4a264d3518655ecabbfb63cef125a4a7
Cathan O'Donnell <cod745@gmail.com>
2016-06-07 23:03:32 +0100

    ADD: Settings option for toggling boot start

--- app/src/main/java/com/elixsr/portforwarder/forwarding/StartForwardingServiceAtBootReceiver.java
+++ app/src/main/java/com/elixsr/portforwarder/forwarding/StartForwardingServiceAtBootReceiver.java
@@ -3,6 +3,9 @@ package com.elixsr.portforwarder.forwarding;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
 
 /**
  * Created by Niall McShane on 07/06/2016.
@@ -13,7 +16,7 @@ public class StartForwardingServiceAtBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) && PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext()).getBoolean("pref_start_on_boot", false)) {
             Intent serviceIntent = new Intent(context, ForwardingService.class);
             context.startService(serviceIntent);
         }
